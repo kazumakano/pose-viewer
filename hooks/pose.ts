b8d795182fc8d4ec76236381d3aef76f49ddddf2
kazumakano <kazumakano@outlook.jp>
2022-10-15 23:43:24 +0900

    apply websocket

--- hooks/pose.ts
+++ hooks/pose.ts
@@ -1,20 +1,23 @@
 import { useEffect, useState } from "react"
 
 
-export default function usePose(): [[number, number, number, number] | null, [number, number, number] | null] {
-  const [ori, setOri] = useState<[number, number, number, number] | null>(null)
-  const [pos, setPos] = useState<[number, number, number] | null>(null)
+export type Orientation = [number, number, number, number]
+export type Position = [number, number, number]
+
+export default function usePose(): [Orientation | null, Position | null] {
+  const [ori, setOri] = useState<Orientation | null>(null)
+  const [pos, setPos] = useState<Position | null>(null)
   const [ws, setWs] = useState<WebSocket | null>(null)
 
   const conn = () => {
     const newWs = new WebSocket(`ws://${process.env.NEXT_PUBLIC_SERVER_HOST}:${process.env.NEXT_PUBLIC_SERVER_PORT}`)
+    newWs.onclose = () => console.log("connection has been closed")
     newWs.onmessage = async (event: MessageEvent<Blob>) => {
       const data = JSON.parse(await event.data.text())
       setOri(data.ori)
       setPos(data.pos)
     }
     newWs.onopen = () => console.log(`connect to ${process.env.NEXT_PUBLIC_SERVER_HOST}:${process.env.NEXT_PUBLIC_SERVER_PORT}`)
-    newWs.onclose = () => console.log("connection has been closed")
 
     setWs(newWs)
   }
