d56ab3b829f26e4ee7740768ef7b6c15f81cf0d6
kazumakano <kazumakano@outlook.jp>
2024-11-24 17:20:35 +0900

    several updates

--- components/scene.tsx
+++ components/scene.tsx
@@ -1,4 +1,3 @@
-import styles from "../styles/Scene.module.css"
 import type { ReactNode } from "react"
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls, Plane } from "@react-three/drei"
@@ -13,27 +12,25 @@ type SceneProps = {
 
 export default function Scene({ children, size }: SceneProps): JSX.Element {
   return (
-    <div className={styles.scene}>
-      <Canvas camera={{ far: 4 * size, fov: 32, position: INIT_CAM_POS }} shadows>
-        <ambientLight intensity={0.5} />
-        <directionalLight
-          castShadow
-          intensity={1}
-          position={[0, size / 2, 0]}
-          shadow-camera-bottom={-size / 2}
-          shadow-camera-far={size / 2 + 1}
-          shadow-camera-left={-size / 2}
-          shadow-camera-right={size / 2}
-          shadow-camera-top={size / 2}
-          shadow-mapSize-height={4 * size}
-          shadow-mapSize-width={4 * size}
-        />
-        <OrbitControls />
-        <Plane args={[size, size]} receiveShadow rotation={[Math.PI / 2, 0, 0]}>
-          <meshStandardMaterial opacity={0.5} side={DoubleSide} transparent />
-        </Plane>
-        {children}
-      </Canvas>
-    </div>
+    <Canvas camera={{ far: 4 * size, fov: 32, position: INIT_CAM_POS }} shadows>
+      <ambientLight intensity={0.5} />
+      <directionalLight
+        castShadow
+        intensity={1}
+        position={[0, size / 2, 0]}
+        shadow-camera-bottom={-size / 2}
+        shadow-camera-far={size / 2 + 1}
+        shadow-camera-left={-size / 2}
+        shadow-camera-right={size / 2}
+        shadow-camera-top={size / 2}
+        shadow-mapSize-height={4 * size}
+        shadow-mapSize-width={4 * size}
+      />
+      <OrbitControls />
+      <Plane args={[size, size]} receiveShadow rotation={[Math.PI / 2, 0, 0]}>
+        <meshStandardMaterial opacity={0.5} side={DoubleSide} transparent />
+      </Plane>
+      {children}
+    </Canvas>
   )
 }
--- hooks/pose.ts
+++ hooks/pose.ts
@@ -1,9 +1,9 @@
-import { useCallback, useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
 export type Orientation = [number, number, number, number]
 export type Position = [number, number, number]
-type PoseData = {
+type Msg = {
   ori: Orientation
   pos: Position
 }
@@ -11,28 +11,18 @@ type PoseData = {
 export default function usePose(): [Orientation | null, Position | null] {
   const [ori, setOri] = useState<Orientation | null>(null)
   const [pos, setPos] = useState<Position | null>(null)
-  const [ws, setWs] = useState<WebSocket | null>(null)
-
-  const conn = useCallback(() => {
-    const newWs = new WebSocket(`ws://${process.env.NEXT_PUBLIC_WS_HOST}:${process.env.NEXT_PUBLIC_WS_PORT}`)
-    newWs.onclose = () => console.log("connection has been closed")
-    newWs.onmessage = async (event: MessageEvent<Blob>) => {
-      const data: PoseData = JSON.parse(await event.data.text())
-      setOri([data.ori[0], data.ori[1], -data.ori[2], -data.ori[3]])
-      setPos([100 * data.pos[0], 100 * data.pos[1], -100 * data.pos[2]])
-    }
-    newWs.onopen = () => console.log(`connected to ${process.env.NEXT_PUBLIC_WS_HOST}:${process.env.NEXT_PUBLIC_WS_PORT}`)
-
-    setWs(newWs)
-  }, [setOri, setPos, setWs])
+  const ws = useRef<WebSocket>()
 
   useEffect(() => {
-    if (ws == null) {
-      conn()
+    console.log(`connecting to server ${process.env.NEXT_PUBLIC_WS_HOST}:${process.env.NEXT_PUBLIC_WS_PORT}`)
+    ws.current = new WebSocket(`ws://${process.env.NEXT_PUBLIC_WS_HOST}:${process.env.NEXT_PUBLIC_WS_PORT}`)
+    ws.current.onmessage = async (ev: MessageEvent<Blob>) => {
+      const msg: Msg = JSON.parse(await ev.data.text())
+      setOri([msg.ori[0], msg.ori[1], -msg.ori[2], -msg.ori[3]])
+      setPos([100 * msg.pos[0], 100 * msg.pos[1], -100 * msg.pos[2]])    // meter to centimeter
     }
-
-    return () => ws?.close()
-  }, [conn, ws])
+    return () => ws.current?.close()
+  }, [setOri, setPos, ws])
 
   return [ori, pos]
 }
